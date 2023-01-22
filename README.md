f9428ea28dfc73691520d98ab39f3dbf361c7905
phuchau.tea <phuchau.tea@gmail.com>
2023-01-22 23:38:04 +0700

    Get IP Address

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 - **PHPMailer**
 - **phpMyAdmin**: 4.6.0 English (Older version compatible with PHP 5.5 to 7.1 and MySQL/MariaDB 5.5 and newer. Was supported until April 1, 2017)
 - **GoogleAuthenticator**: Google Authenticator 2-factor authentication
+- **GetIPAddress**: Get IP address
---
+++ getIPAddress.php
@@ -0,0 +1,31 @@
+<?php
+function getIPAddress(){
+	if(isset($_SERVER)){
+		if(isset($_SERVER["HTTP_X_FORWARDED_FOR"])){
+			$ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+			if(strpos($ip, ",")){
+				$exp_ip = explode(",", $ip);
+				$ip = $exp_ip[0];
+			}
+		}else if (isset($_SERVER["HTTP_CLIENT_IP"])){
+			$ip = $_SERVER["HTTP_CLIENT_IP"];
+		}else{
+			$ip = $_SERVER["REMOTE_ADDR"];
+		}
+	}else{
+		if(getenv('HTTP_X_FORWARDED_FOR')){
+			$ip = getenv('HTTP_X_FORWARDED_FOR');
+			if(strpos($ip, ",")){
+				$exp_ip = explode(",", $ip);
+				$ip = $exp_ip[0];
+			}
+		}else if (getenv('HTTP_CLIENT_IP')){
+			$ip = getenv('HTTP_CLIENT_IP');
+		}else{
+			$ip = getenv('REMOTE_ADDR');
+		}
+	}
+	return $ip;
+}
+echo getIPAddress();
+?>
